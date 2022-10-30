926b88f19247adde16d3a2d21fe955e83890be25
WizarDRW <wnokroot@gmail.com>
2022-10-30 13:22:46 +0300

    Calendar started.

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import WoGroupCalendar from "./components/GroupCalendar.vue"
+</script>
+
+<template>
+  <wo-group-calendar></wo-group-calendar>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/GroupCalendar.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="wo-container">
+        <div class="wo-page">
+            <div class="wo-head">
+                <div class="wo-time">Time Head</div>
+                <div class="wo-category">Categories of Name</div>
+            </div>
+            <div class="wo-body">
+                <div class="wo-time">01:00</div>
+                <div class="wo-category">Events</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        resources: {
+            type: Array
+        }
+    },
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<style>
+    
+</style>
