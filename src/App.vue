c2c03761785513604234b15f7a4c224ba3ddcfb8
WizarDRW <wnokroot@gmail.com>
2022-11-13 23:59:41 +0300

    .

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,123 @@
 <script setup lang="ts">
 import WoGroupCalendar from "./components/GroupCalendar.vue"
+let events = [
+        { name: "Category1" },
+        {
+          name: "Category2",
+          events: [
+            {
+              name: "Deneme",
+              start: {
+                date: "2022-10-30",
+                time: "10:00",
+                year: 2022,
+                month: 10,
+                day: 30,
+                weekday: 0,
+                hour: 9,
+                minute: 28,
+                hasDay: true,
+                hasTime: true,
+                past: false,
+                present: true,
+                future: false,
+              },
+              end: {
+                date: "2022-10-30",
+                time: "11:00",
+                year: 2022,
+                month: 10,
+                day: 30,
+                weekday: 0,
+                hour: 9,
+                minute: 35,
+                hasDay: true,
+                hasTime: true,
+                past: false,
+                present: true,
+                future: false,
+              },
+            },
+            {
+              name: "Deneme",
+              start: {
+                date: "2022-10-30",
+                time: "10:00",
+                year: 2022,
+                month: 10,
+                day: 30,
+                weekday: 0,
+                hour: 10,
+                minute: 5,
+                hasDay: true,
+                hasTime: true,
+                past: false,
+                present: true,
+                future: false,
+              },
+              end: {
+                date: "2022-10-30",
+                time: "11:00",
+                year: 2022,
+                month: 10,
+                day: 30,
+                weekday: 0,
+                hour: 10,
+                minute: 10,
+                hasDay: true,
+                hasTime: true,
+                past: false,
+                present: true,
+                future: false,
+              },
+            },
+            {
+              name: "Deneme",
+              start: {
+                date: "2022-10-30",
+                time: "10:00",
+                year: 2022,
+                month: 10,
+                day: 30,
+                weekday: 0,
+                hour: 10,
+                minute: 10,
+                hasDay: true,
+                hasTime: true,
+                past: false,
+                present: true,
+                future: false,
+              },
+              end: {
+                date: "2022-10-30",
+                time: "11:00",
+                year: 2022,
+                month: 10,
+                day: 30,
+                weekday: 0,
+                hour: 10,
+                minute: 15,
+                hasDay: true,
+                hasTime: true,
+                past: false,
+                present: true,
+                future: false,
+              },
+            },
+          ],
+        },
+        {
+          name: "Category3",
+          events: [],
+        },
+      ];
 function time(params: any) {
   console.log(params);
 }
 </script>
 
 <template>
-  <wo-group-calendar @click:time="time"></wo-group-calendar>
+  <wo-group-calendar :resources="events" @click:time="time"></wo-group-calendar>
 </template>
 
 <style scoped>
